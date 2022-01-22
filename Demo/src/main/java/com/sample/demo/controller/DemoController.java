dcfc0296525f7973cf7a19d0ffd7574601ab048c
Uday Kumar <Uday Kumar@DESKTOP-J057D2Q>
2022-01-22 15:29:16 +0530

    My First Commit..!

---
+++ Demo/src/main/java/com/sample/demo/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.sample.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping(value = "/healthCheck")
+	public String healthCheck() 
+	{
+		return "Up....!";
+	}
+
+}
